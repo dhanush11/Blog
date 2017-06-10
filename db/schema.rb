43168a58bf2d1cdd7da2fdbba5daee450ec9aa78
YOUR NAME <YOUR@EMAIL.com>
2017-06-10 17:50:56 +0530

    subscriber added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610113737) do
+ActiveRecord::Schema.define(version: 20170610121652) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20170610113737) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
